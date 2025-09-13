d770a3533e4acfea7ffc209b0e66dc9597867605
Akshay-prakash-codes <akshayprakash.tb2023@vitstudent.ac.in>
2025-09-13 15:40:50 +0530

    initial commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { getAllNotes, createNote, getNoteById, updateNote, deleteNote} from "../controller/notesController.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+export default router;
